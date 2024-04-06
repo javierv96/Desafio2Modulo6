89218e079390522973f8f8add58b4bfbfd6fac78
Javier Vergara <ja.vergarac96@gmail.com>
2024-04-06 20:32:27 -0300

    agregar y elimianr productos al carrito

--- assets/js/front.js
+++ assets/js/front.js
@@ -2,10 +2,16 @@ $(document).ready(function () {
     $('.producto').click(function (e) {
         e.preventDefault();
         $(this).addClass('seleccionado');
+        var nombreProducto = $(this).data('nombre');
+        $.post('/agregar-al-carrito', { nombre: nombreProducto }, function (data) {
+        });
     });
 
     $('.producto').dblclick(function (e) {
         e.preventDefault();
         $(this).removeClass('seleccionado');
+        var nombreProducto = $(this).data('nombre');
+        $.post('/quitar-del-carrito', { nombre: nombreProducto }, function (data) {
+        });
     });
 });
--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const productos = [
-    { nombre: 'banana', imagen: 'banana.png' },
-    { nombre: 'cebollas', imagen: 'cebollas.png' },
-    { nombre: 'pimenton', imagen: 'pimenton.png' },
-    { nombre: 'papas', imagen: 'papas.png' },
-    { nombre: 'lechuga', imagen: 'lechuga.png' },
-    { nombre: 'tomate', imagen: 'tomate.png' }
+    { nombre: 'banana', imagen: 'banana.png', seleccionado:false },
+    { nombre: 'cebollas', imagen: 'cebollas.png', seleccionado:false },
+    { nombre: 'pimenton', imagen: 'pimenton.png', seleccionado:false },
+    { nombre: 'papas', imagen: 'papas.png', seleccionado:false },
+    { nombre: 'lechuga', imagen: 'lechuga.png', seleccionado:false },
+    { nombre: 'tomate', imagen: 'tomate.png', seleccionado:false }
 ];
 
 let carrito = [];
 
-app.set("view engine", "handlebars"); 
+app.set("view engine", "handlebars");
 
 app.engine('handlebars', exphbs.engine({
     extname: '.handlebars', // Extensiones de los archivos de plantillas
@@ -30,15 +31,48 @@ app.use("/jquery", express.static(__dirname + "/node_modules/jquery/dist"));
 app.use('/css', express.static(__dirname + '/assets/css'));
 app.use('/front', express.static(__dirname + '/assets/js'));
 
-app.get("/", function(req, res) {
-    res.render("Main", {
-        productos: productos,
-        carrito: carrito
-    });
+// Configura body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/agregar-al-carrito', (req, res) => {
+    const productoNombre = req.body.nombre;
+    const producto = productos.find(p => p.nombre === productoNombre);
+    if (producto) {
+        if (!producto.seleccionado) {
+            producto.seleccionado = true;
+            carrito.push(producto);
+            console.log(producto.seleccionado);
+        } else {
+            console.log("El producto ya está en el carrito: " + productoNombre);
+        }
+        
+    }
 });
 
-app.get("/menu", function(req, res) {
-    res.render("menu", {
+app.post('/quitar-del-carrito', (req, res) => {
+    const productoNombre = req.body.nombre;
+    const producto = productos.find(p => p.nombre === productoNombre);
+    if (producto) {
+        const index = carrito.indexOf(producto);
+        if (index !== -1) {
+            carrito.splice(index, 1);
+            producto.seleccionado = false; // Cambiar el estado del producto a no seleccionado
+            console.log("Producto eliminado del carrito: " + productoNombre);
+            res.send('Producto eliminado del carrito.');
+            console.log(producto.seleccionado);
+        } else {
+            console.log("El producto no se encontró en el carrito: " + productoNombre);
+            res.send('El producto no se encontró en el carrito.');
+        }
+    } else {
+        console.log("El producto no existe: " + productoNombre);
+        res.send('El producto no existe.');
+    }
+});
+
+app.get("/", function (req, res) {
+    res.render("Main", {
+        productos: productos,
         carrito: carrito
     });
 });
